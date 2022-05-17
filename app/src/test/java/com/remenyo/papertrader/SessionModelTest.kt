770e4d343aa1bb0597829853a54b044eee5ad708
Benana6 <szeman.benjamin@gmail.com>
2022-05-17 08:19:05 +0200

    FireBase mock Error help

--- app/src/test/java/com/remenyo/papertrader/SessionModelTest.kt
+++ app/src/test/java/com/remenyo/papertrader/SessionModelTest.kt
@@ -1,6 +1,8 @@
 package com.remenyo.papertrader
 
 import android.os.PerformanceHintManager
+import com.google.firebase.ktx.Firebase
+import com.remenyo.papertrader.db.realtime.RealtimeDB
 import com.remenyo.papertrader.db.realtime.RealtimeDBRepo
 import com.remenyo.papertrader.db.room.CandleRepo
 import io.mockk.*
@@ -13,33 +15,31 @@ import org.mockito.Mockito
 import org.mockito.Mockito.*
 
 class SessionModelTest {
+    val candles: List<OHLCV> = listOf<OHLCV>(
+        OHLCV(0,0.0,1.0,0.0,1.0,1.0),
+        OHLCV(1,1.0,2.0,0.0,1.0,1.0),
+        OHLCV(2,1.0,3.0,1.0,3.0,1.0),
+        OHLCV(3,3.0,6.0,3.0,6.0,1.0),
+        OHLCV(4,7.0,9.0,7.0,9.0,1.0),
+        OHLCV(5,7.0,7.0,2.0,3.0,1.0),
+        OHLCV(6,5.0,6.0,3.0,4.0,1.0),
+        OHLCV(7,4.0,5.0,1.0,3.0,1.0),
+        OHLCV(8,3.0,6.0,3.0,6.0,1.0),
+        OHLCV(9,7.0,9.0,7.0,9.0,1.0),
+        OHLCV(10,7.0,7.0,2.0,3.0,1.0)
+    )
 
+    var sd= SessionData(
+        startTS = 0,
+        endTS = 10,
+        currentTS = 0,
+        id = "test"
+    )
     @Test
     fun isTimeStampDirtyTest() = runBlocking{
         mockkObject(SessionModel)
         mockkObject(RealtimeDBRepo)
         mockkObject(CandleRepo)
-        
-        val candles: List<OHLCV> = listOf<OHLCV>(
-            OHLCV(0,0.0,1.0,0.0,1.0,1.0),
-            OHLCV(1,1.0,2.0,0.0,1.0,1.0),
-            OHLCV(2,1.0,3.0,1.0,3.0,1.0),
-            OHLCV(3,3.0,6.0,3.0,6.0,1.0),
-            OHLCV(4,7.0,9.0,7.0,9.0,1.0),
-            OHLCV(5,7.0,7.0,2.0,3.0,1.0),
-            OHLCV(6,5.0,6.0,3.0,4.0,1.0),
-            OHLCV(7,4.0,5.0,1.0,3.0,1.0),
-            OHLCV(8,3.0,6.0,3.0,6.0,1.0),
-            OHLCV(9,7.0,9.0,7.0,9.0,1.0),
-            OHLCV(10,7.0,7.0,2.0,3.0,1.0)
-        )
-
-        var sd= SessionData(
-            startTS = 0,
-            endTS = 10,
-            currentTS = 0,
-            id = "test"
-        )
 
         coEvery {
             RealtimeDBRepo.fetchSession(sd.id)
@@ -60,5 +60,44 @@ class SessionModelTest {
         assertEquals(SessionModel.currentTimestamp,1)
 
         coVerify { RealtimeDBRepo.updateSessionCurrentTS(SessionData(currentTS = 1)) }
+    }
+
+    @Test
+    fun isSessionDirtyTest() = runBlocking {
+        mockkObject(SessionModel)
+        mockkObject(RealtimeDBRepo)
+        mockkObject(RealtimeDB)
+        mockkObject(CandleRepo)
+
+        var order = Order(
+            id="testOrder",
+            sep = 0.0,
+            multi = 1.0,
+            trail = 0.0
+        )
+
+        coEvery {
+            RealtimeDBRepo.fetchSession(sd.id)
+        } returns sd
+
+        coEvery {
+            CandleRepo.getRange(sd.startTS,sd.endTS)
+        } returns candles
+
+        coEvery {
+            RealtimeDBRepo.updateSession(any(),any())
+        }returns true
+
+        coEvery {
+            RealtimeDB.makeDocWithRandomID(any())
+        } returns null
+
+
+        SessionModel.Command.addMarketOrderToSession(order)
+
+        SessionModel.saveSession()
+
+        coVerify { RealtimeDBRepo.updateSession(any(),any()) }
+
     }
 }
